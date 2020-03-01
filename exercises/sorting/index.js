8c340e2407bca8117ff45d08eac733be22cf6fcb
Fake Person <afakeid@yahoo.com>
2020-02-29 21:05:18 -0800

    finish

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,55 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
-
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (i !== min) {
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
-
+    if (arr.length <= 1) {
+        return arr;
+    }
+    const center = Math.floor(arr.length / 2);
+    return merge(mergeSort(arr.slice(0, center)), mergeSort(arr.slice(center)));
 }
 
 function merge(left, right) {
-
+    let arr = [];
+    while (left.length > 0 && right.length > 0) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    arr.push(...left);
+    arr.push(...right);
+    return arr;
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
